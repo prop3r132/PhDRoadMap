33073b569be7da0bc37683db0a798fcedc26a9ba
prop3r132 <rahatrumi6671@gmail.com>
2024-07-31 12:48:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/PhDRoadMap.html');
-    res.sendFile(__dirname + '/public/node_mon');
+    // res.sendFile(__dirname + '/public/node_mon');
 });
 
 // Use the API router
